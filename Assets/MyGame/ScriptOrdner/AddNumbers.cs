a61719ae8c402191b72ef3fd8492ddf2009365fd
unknown <stefanie.zwingenberger03@htl-salzburg.ac.at>
2019-11-12 09:52:29 +0100

    delete lines in Visual Studio

--- Assets/MyGame/ScriptOrdner/AddNumbers.cs
+++ Assets/MyGame/ScriptOrdner/AddNumbers.cs
@@ -1,8 +1,7 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour
+public class AddNumbersScript : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
@@ -25,9 +24,4 @@ public class NewBehaviourScript : MonoBehaviour
 
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
